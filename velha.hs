6f1405b2c380a5b192b8f2c7d8641e67682a6b5a
Gabriela Moreira <gabrielamoreira05@gmail.com>
2016-12-05 13:43:31 -0200

    Add files via upload

--- velha.hs
+++ velha.hs
@@ -8,16 +8,15 @@ instance Show Jogada where
     show X = "X"
     show B = " "
 
---type Tabuleiro [[Jogada]]
 
-iniciaTabuleiro = replicate 3 (replicate 3 B)
+mostraTabuleiro xs = "\n" ++ mostraLinha (xs!!0) (fmap show [1..3]) ++ divisoria ++ mostraLinha (xs!!1) (fmap show [4..6]) ++ divisoria ++ mostraLinha (xs!!2) (fmap show [7..9]) ++ "\n\n"
 
 mostraLinha [x] [n] = " " ++ (if x==B then n else show x)
 mostraLinha (x:xs) (n:ns) = " " ++ (if x==B then n else show x) ++ " |" ++ mostraLinha xs ns
 
 divisoria = "\n ----------\n"
 
-mostraTabuleiro xs = "\n" ++ mostraLinha (xs!!0) (fmap show [1..3]) ++ divisoria ++ mostraLinha (xs!!1) (fmap show [4..6]) ++ divisoria ++ mostraLinha (xs!!2) (fmap show [7..9]) ++ "\n\n"
+iniciaTabuleiro = replicate 3 (replicate 3 B)
 
 vitoria y ts = olha y ts || olha y (transpose ts) || olha y (diagonais ts)
 
@@ -26,13 +25,27 @@ olha y (x:xs) = (all (==y) x) || olha y xs
 
 diagonais [a,b,c] = ([[a!!0] ++ [b!!1] ++ [c!!2],[a!!2] ++ [b!!1] ++ [c!!0]])
 
-vazio l c ts = ts!!l!!c==B
+vazio l c ts = ts!!l!!c == B
+
+qtdBrancos ts = length (brancos ts)
+
+jogadaVazia ts e = ts!!(div e 3)!!(mod e 3) == B
+
+brancos ts = filter(jogadaVazia ts) [0..8]
+
+alterna X = O
+alterna O = X
+
+soma [] = 0
+soma (x:xs) = x + soma xs
 
 insereJogada n l c ts = if (vazio l c ts) then Just [insereJogada' n l c ts 0 0, insereJogada' n l c ts 1 0, insereJogada' n l c ts 2 0] else Nothing
 insereJogada' _ _ _ _ _ 3 = []
 insereJogada' n l c ts i j = if (i==l && j==c) then (n:insereJogada' n l c ts i (j+1)) else ts!!i!!j:insereJogada' n l c ts i (j+1)
 
 jogoPvP = jogar1 iniciaTabuleiro
+
+jogoVsBot = jogarP iniciaTabuleiro
 
 jogar1 ts = do
     putStr(mostraTabuleiro ts)
@@ -42,10 +55,11 @@ jogar1 ts = do
     let sucesso = isJust jogada
     if sucesso then do
         let novoTabuleiro = fromJust jogada
-        if vitoria O novoTabuleiro then do {putStrLn ("\nVitoria do jogador 1!\n");putStrLn (mostraTabuleiro novoTabuleiro);return()} else jogar2 novoTabuleiro
-    else do
-        putStrLn ("Jogada invalida, tente novamente.\n")
-        jogar1 ts
+        if vitoria O novoTabuleiro then do {putStrLn ("\nVitoria do jogador 1!\n");putStrLn (mostraTabuleiro novoTabuleiro);return()}
+        else 
+            if qtdBrancos novoTabuleiro == 0 then do {putStrLn ("\nDeu velha!\n");putStrLn (mostraTabuleiro novoTabuleiro);return()} 
+            else jogar2 novoTabuleiro
+    else do {putStrLn ("Jogada invalida, tente novamente.\n");jogar1 ts}
 
 jogar2 ts = do
     putStr(mostraTabuleiro ts)
@@ -55,12 +69,11 @@ jogar2 ts = do
     let sucesso = isJust jogada
     if sucesso then do
         let novoTabuleiro = fromJust jogada
-        if vitoria X novoTabuleiro then do {putStrLn ("\nVitoria do jogador 2!\n");putStrLn (mostraTabuleiro novoTabuleiro);return()} else jogar1 novoTabuleiro
-    else do
-        putStrLn ("Jogada invalida, tente novamente.\n")
-        jogar2 ts
-
-jogoVsBot = jogarP iniciaTabuleiro
+        if vitoria X novoTabuleiro then do {putStrLn ("\nVitoria do jogador 2!\n");putStrLn (mostraTabuleiro novoTabuleiro);return()} 
+        else 
+            if qtdBrancos novoTabuleiro == 0 then do {putStrLn ("\nDeu velha!\n");putStrLn (mostraTabuleiro novoTabuleiro);return()} 
+            else jogar1 novoTabuleiro
+    else do {putStrLn ("Jogada invalida, tente novamente.\n");jogar2 ts}
 
 jogarP ts = do
     putStr(mostraTabuleiro ts)
@@ -70,27 +83,13 @@ jogarP ts = do
     let sucesso = isJust jogada
     if sucesso then do
         let novoTabuleiro = fromJust jogada
-        if vitoria X novoTabuleiro then do {putStrLn ("\nVitoria do jogador 1!\n");putStrLn (mostraTabuleiro novoTabuleiro);return()} else jogarIA novoTabuleiro
-    else do
-        putStrLn ("Jogada invalida, tente novamente.\n")
-        jogarP ts
-
-
---linhaCritica ls = lenght (filter (==X) ls) == 2 && lenght (filter (==B) ls) == 1 
-
-qtdBrancos ts = length (brancos ts)
-
-jogadaVazia ts e = ts!!(div e 3)!!(mod e 3)==B
-
-brancos ts = filter(jogadaVazia ts) [0..8]
-
-fat 1 = 1
-fat n = n * fat(n-1)
+        if vitoria X novoTabuleiro then do {putStrLn ("\nVitoria do jogador 1!\n");putStrLn (mostraTabuleiro novoTabuleiro);return()} 
+        else 
+            if qtdBrancos novoTabuleiro == 0 then do {putStrLn ("\nDeu velha!\n");putStrLn (mostraTabuleiro novoTabuleiro);return()} 
+            else jogarIA novoTabuleiro
+    else do {putStrLn ("Jogada invalida, tente novamente.\n");jogarP ts}
 
 jogarIA ts = do
-    --let b = snd (melhorJogada ts O)
-    --let jogada = insereJogada O (div b 3) (mod b 3) ts
-    --let novoTabuleiro = fromJust jogada
     let novoTabuleiro = tabuleiroResultante ts O
     if vitoria O novoTabuleiro then do 
         putStrLn ("\nVitoria do Computador!\n")
@@ -99,29 +98,20 @@ jogarIA ts = do
     else do
         jogarP novoTabuleiro
 
-geraJogadas j ts = geraJogadas' j ts (brancos ts) 
-geraJogadas' _ _ [] = []
-geraJogadas' j ts (b:bs) = fromJust (insereJogada j (div b 3) (mod b 3) ts):geraJogadas' j ts bs
+jogadasPossiveis j ts = jogadasPossiveis' j ts (brancos ts) 
+jogadasPossiveis' _ _ [] = []
+jogadasPossiveis' j ts (b:bs) = fromJust (insereJogada j (div b 3) (mod b 3) ts):jogadasPossiveis' j ts bs
 
-alterna X = O
-alterna O = X
-
---tabuleiroResultante ts j = (geraJogadas j ts)!!(snd (melhorJogada ts j))
 tabuleiroResultante ts j = (fromJust (insereJogada j (div i 3) (mod i 3) ts))
-    where i = (brancos ts)!!(melhorJogada ts j)
+    where i = if qtdBrancos ts == 8 then primeiraJogada ts else (brancos ts)!!(melhorJogada ts j)
 
 melhorJogada [] _ = 0
-melhorJogada ts j = snd (maior (map (jogadas j) (geraJogadas j ts)))
+melhorJogada ts j =  snd (maior (map (jogadas j) (jogadasPossiveis j ts)))
 melhorJogada' [] _ = 0
-melhorJogada' ts j = maiorRelativo (map (jogadas j) (geraJogadas j ts))
+melhorJogada' ts j = soma (map (jogadas j) (jogadasPossiveis j ts))
 
 jogadas _ [] = -1
-jogadas j ts = if vitoria X ts then 0 else (if vitoria O ts then (fat(qtdBrancos ts)) else melhorJogada' ts (alterna j))
-
-
-maiorRelativo [x] = x
-maiorRelativo (x:xs) = if x > m || x == 0 then x else m
-    where m = maiorRelativo xs 
+jogadas j ts = if vitoria X ts then (-1*10^(qtdBrancos ts)) else (if vitoria O ts then (10^(qtdBrancos ts)) else melhorJogada' ts (alterna j))
 
 maior xs = maior' xs 0
 maior' [] _ = (-1,0)
@@ -129,13 +119,17 @@ maior' [x] e = (x,e)
 maior' (x:xs) e = if x > fst(m) then (x,e) else m
     where m = maior' xs (e+1)
 
+primeiraJogada ts = if jogadaVazia ts 4 then 4 else 2
 
+-- Testes:
 t0 = (fromJust (insereJogada X (div 0 3) (mod 0 3) iniciaTabuleiro))
 t1 = (fromJust (insereJogada O (div 1 3) (mod 1 3) t0))
 t2 = (fromJust (insereJogada X (div 6 3) (mod 6 3) t1))
 t3 = (fromJust (insereJogada O (div 7 3) (mod 7 3) t2))
 t4 = (fromJust (insereJogada X (div 5 3) (mod 5 3) t3))
 
- 
+jt4 = (map (jogadas O) (jogadasPossiveis O t4))
+
+
 
 
